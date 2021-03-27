fad2908c52eebfd0cbbe0397f4803948bc78c2af
Sierra <55088939+evelynsierra@users.noreply.github.com>
2021-03-27 16:41:47 +0700

    soal 1e

--- soal1/soal1.sh
+++ soal1/soal1.sh
@@ -18,3 +18,18 @@ grep -P -o "(?<=ERROR ).*(?=\()" syslog.log |sort|uniq -c|sort -nr|while read te
 do
  ambil_text $text;
 done
+
+#soal 1e
+#buat file user_statistic.csv
+echo "Username,INFO,ERROR\n" > "user_statistic.csv"
+#filter dari error, info
+grep -P -o "(ERROR|INFO)(.*)" syslog.log|sort|uniq | while read text
+do
+#dapatkan username,info,error
+username=($(grep -w "(?<=\().*(?=\))" "$text"|sort|uniq))
+info=($(grep -w "(INFO)" "$text"|sort|uniq -c))
+error=($(grep -w "(ERROR)" "$text"|sort|uniq -c))
+done
+
+printf "%s,%d,%d" "$username" "$info" "$error" >> "user_statistic.csv"
+
